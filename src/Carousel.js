bd22a580bbf8c75f986bda0bc0b94d60ee8bc109
Loc Tran <loc_tran@datahouse.com>
2021-10-19 22:24:22 +0700

    :wrench: Initial stylelint setup

--- src/Carousel.js
+++ src/Carousel.js
@@ -5,8 +5,13 @@ import CarouselSlide from './CarouselSlide';
 
 class Carousel extends React.PureComponent {
   static propTypes = {
+    defaultImgHeight: CarouselSlide.propTypes.imgHeight,
     slides: PropTypes.arrayOf(PropTypes.shape(CarouselSlide.propTypes))
       .isRequired,
+  };
+
+  static defaultProps = {
+    defaultImgHeight: CarouselSlide.defaultProps.imgHeight,
   };
 
   state = {
@@ -16,7 +21,7 @@ class Carousel extends React.PureComponent {
   handlePrevClick = () => {
     const { slides } = this.props;
     this.setState(({ slideIndex }) => ({
-      slideIndex: (slideIndex + slides.length - 1) % slides.length, // 1
+      slideIndex: (slideIndex + slides.length - 1) % slides.length,
     }));
   };
 
@@ -28,11 +33,14 @@ class Carousel extends React.PureComponent {
   };
 
   render() {
-    const { slides, ...rest } = this.props;
+    const { defaultImgHeight, slides, ...rest } = this.props;
 
     return (
       <div {...rest}>
-        <CarouselSlide {...slides[this.state.slideIndex]} /* 1 */ />
+        <CarouselSlide
+          imgHeight={defaultImgHeight}
+          {...slides[this.state.slideIndex]}
+        />
         <CarouselButton data-action='prev' onClick={this.handlePrevClick}>
           Prev
         </CarouselButton>
